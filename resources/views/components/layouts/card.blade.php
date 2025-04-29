f9019117eac1ddacd0974986394506431788806e
jhshin <jhshin@gmediasoft.com>
2025-04-29 13:30:31 +0900

    컴포넌트에 아이콘 추가하기

--- resources/views/components/layouts/card.blade.php
+++ resources/views/components/layouts/card.blade.php
@@ -1,5 +1,10 @@
+@props(['title', 'icon' => null])
+
 <div class="card" style="border:1px solid #ccc; padding: 1rem; margin-bottom: 1rem;">
     <div class="card-header" style="font-weight: bold;">
+        @if ($icon)
+            <span style="margin-right: 0.5rem;">{!! $icon !!}</span>
+        @endif
         {{ $title }}
     </div>
     <div class="card-body">
--- resources/views/www/test-card.blade.php
+++ resources/views/www/test-card.blade.php
@@ -3,14 +3,29 @@
 @section('title', 'Card 컴포넌트 테스트')
 
 @section('content')
-    <x-layouts.card title="테스트 카드입니다">
-        <p>이것은 카드 컴포넌트의 슬롯으로 들어간 내용입니다.</p>
+    <x-layouts.card title="공지사항" :icon="'📢'">
+        <p>최신 공지사항을 확인하세요.</p>
     </x-layouts.card>
 
-    <x-layouts.card title="두 번째 카드">
-        <ul>
-            <li>항목 1</li>
-            <li>항목 2</li>
-        </ul>
+    <x-layouts.card title="문의하기" :icon="'✉️'">
+        <p>궁금한 점이 있으면 문의해주세요.</p>
     </x-layouts.card>
+
+    {{--    반복문으로 카드 여러 개 렌더링하기--}}
+    @php
+        $posts = [
+            ['title' => 'Laravel 11 출시', 'body' => '최신 버전은 많은 변화가 있습니다.', 'icon' => '🚀'],
+            ['title' => 'Blade 컴포넌트 소개', 'body' => '재사용 가능한 UI를 만들어보세요.', 'icon' => '🧩'],
+            ['title' => '컨트롤러 정리법', 'body' => 'RESTful 방식으로 깔끔하게 구성하세요.', 'icon' => '🗂️'],
+        ];
+    @endphp
+
+    @foreach ($posts as $post)
+        <x-layouts.card :title="$post['title']" :icon="$post['icon']">
+            {{ $post['body'] }}
+        </x-layouts.card>
+    @endforeach
+
+
 @endsection
+
